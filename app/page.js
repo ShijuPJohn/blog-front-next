4bfeaf35f01cf837dc491489f1933542751c5c7f
ShijuPJohn <shijupjohn4u@gmail.com>
2023-05-08 19:56:02 +0530

    add sitemap.xml. add meta description to homepage.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import styles from './page.module.css'
 import PostCard from "@/app/post_card/post_card";
 import {fetchURL} from "@/app/constants";
+import React from "react";
+
 async function getPosts() {
-    const response = await fetch(`${fetchURL}/posts`,{ cache: 'no-store' });
+    const response = await fetch(`${fetchURL}/posts`, {cache: 'no-store'});
 // ,{ cache: 'no-store' }
     if (!response.ok) {
         throw new Error('fetch error');
@@ -14,17 +16,31 @@ export const metadata = {
     icons: {
         icon: '/favicon.png',
     },
-    title:'ThinkPython.dev'
+    title: 'ThinkPython.dev'
 };
 
 export default async function Home() {
     const posts = await getPosts();
+    const metaDescr = "Here you will find a ton of resources to help you master this powerful and versatile programming language, including step-by-step tutorials, and expert tips."
     return (
-        <main className={styles.main}>
-            {posts && posts.map(post => (
-                <PostCard post={post} createdDate={(new Date(post.time_created)).toLocaleDateString()} key={post.id}/>
+        <>
+            <meta
+                name="description"
+                content={metaDescr}/>
+            <meta
+                property="og:description"
+                content={metaDescr}/>
+            <meta
+                property="og:image"
+                content="/logo.png"
+            />
+            <main className={styles.main}>
+                {posts && posts.map(post => (
+                    <PostCard post={post} createdDate={(new Date(post.time_created)).toLocaleDateString()}
+                              key={post.id}/>
 
-            ))}
-        </main>
+                ))}
+            </main>
+        </>
     )
 }
